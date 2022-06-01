b9f2d16c8c2f1f955e0f568f61213d1ac6672bcc
Alexey Efimov <lex.efimov@gmail.com>
2022-06-01 21:48:28 +0300

    Optimization

--- CustomTabBar/MainTabBarController.swift
+++ CustomTabBar/MainTabBarController.swift
@@ -17,48 +17,56 @@ class MainTabBarController: UITabBarController {
     }
     
     private func generateTabBar() {
-        let homeViewController = HomeViewController()
-        let personViewController = PersonViewController()
-        let settingsViewController = SettingsViewController()
-        
-        let homeImage = UIImage(systemName: "house.fill")!
-        let personImage = UIImage(systemName: "person.fill")!
-        let settingsImage = UIImage(systemName: "slider.horizontal.3")!
-        
-       
         viewControllers = [
-            generateVC(viewController: homeViewController, title: "Home", image: homeImage),
-            generateVC(viewController: personViewController, title: "Personal Info", image: personImage),
-            generateVC(viewController: settingsViewController, title: "Settings", image: settingsImage)
+            generateVC(
+                viewController: HomeViewController(),
+                title: "Home",
+                image: UIImage(systemName: "house.fill")
+            ),
+            generateVC(
+                viewController: PersonViewController(),
+                title: "Profile",
+                image: UIImage(systemName: "person.fill")
+            ),
+            generateVC(
+                viewController: SettingsViewController(),
+                title: "Settings",
+                image: UIImage(systemName: "slider.horizontal.3")
+            )
         ]
     }
     
-    private func generateVC(viewController: UIViewController, title: String, image: UIImage) -> UIViewController {
-        let viewController = viewController
+    private func generateVC(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
         viewController.tabBarItem.title = title
         viewController.tabBarItem.image = image
         return viewController
     }
     
     private func setTabBarAppearance() {
-        tabBar.tintColor = .tabBarItemMain()
-        tabBar.barTintColor = .clear
-        tabBar.backgroundColor = .clear
-        
-        let roundLayer = CAShapeLayer()
-        
         let positionOnX: CGFloat = 10
         let positionOnY: CGFloat = 14
         let width = tabBar.bounds.width - positionOnX * 2
         let height = tabBar.bounds.height + positionOnY * 2
-        roundLayer.fillColor = UIColor.mainWhite().cgColor
   
+        let roundLayer = CAShapeLayer()
+        roundLayer.fillColor = UIColor.mainWhite().cgColor
         
-        let bezierPath = UIBezierPath(roundedRect: CGRect(x: positionOnX, y: tabBar.bounds.minY - positionOnY, width: width, height: height), cornerRadius: height / 2)
+        let bezierPath = UIBezierPath(
+            roundedRect: CGRect(
+                x: positionOnX,
+                y: tabBar.bounds.minY - positionOnY,
+                width: width,
+                height: height
+            ),
+            cornerRadius: height / 2
+        )
+        
         roundLayer.path = bezierPath.cgPath
         
+        tabBar.tintColor = .tabBarItemMain()
+        tabBar.barTintColor = .clear
+        tabBar.backgroundColor = .clear
         tabBar.layer.insertSublayer(roundLayer, at: 0)
-        
         tabBar.itemWidth = width / 5
         tabBar.itemPositioning = .centered
         tabBar.unselectedItemTintColor = .tabBarItemLight()
--- CustomTabBar/UIColorExtension.swift
+++ CustomTabBar/UIColorExtension.swift
@@ -10,12 +10,12 @@ import UIKit
 extension UIColor {
    
     static func tabBarItemMain() -> UIColor {
-        #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+        #colorLiteral(red: 0.3254901961, green: 0.1058823529, blue: 0.5764705882, alpha: 1)
     }
     static func mainWhite() -> UIColor {
         #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
     static func tabBarItemLight() -> UIColor {
-        #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 0.4852028146)
+        #colorLiteral(red: 0.3254901961, green: 0.1058823529, blue: 0.5764705882, alpha: 0.4852028146)
     }
 }
